5e1285feea128dacd9b87024372a0c3ca16fae51
andriysoroka <dree0103@gmail.com>
2018-05-23 17:20:11 +0300

    first

---
+++ README.md
@@ -0,0 +1,21 @@
+***
+
+**Version 1.0.0**
+
+## RUN THE APLICATION
+
+to install node modules
+```
+npm install
+```
+
+to run your bundle.js
+```
+npm run build 
+```
+run the local server
+```
+node server.js
+```
+
+and open in your browser http://localhost:9000
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+
+const extractSass = new ExtractTextPlugin({
+    filename: 'main.css',
+    disable: process.env.NODE_ENV === 'development'
+});
+
+  module.exports = {
+    entry: './src/scripts/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+   module: {
+     rules: [
+       {
+         test: /\.(css|scss)$/,
+         use: extractSass.extract({ 
+             use: [{
+               loader:  'css-loader'
+             }, {
+                 loader: 'sass-loader'
+             }],
+             fallback: 'style-loader'
+         })
+         
+       },
+       {
+         test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+         use: [{
+           loader: 'file-loader',
+           options: {
+             name: '[name].[ext]',
+             outputPath: 'fonts/',    
+             publicPath: '../'     
+           }
+         }]
+       }
+     ]
+   },
+  plugins: [
+       extractSass
+  ]
+  };
